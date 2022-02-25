2cf45a408b000b2ae3368663973a32022819e353
Jonas Bäck <jonas.c.back@gmail.com>
2022-02-25 15:50:56 +0200

    Added handler for unknown endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,8 @@ import feedbackRouter from './routers/feedback';
 const app = express();
 app.use(express.json());
 app.use('/api/feedback', feedbackRouter);
+app.use((request, response) => {
+  response.sendStatus(404);
+});
 
 export default app;
